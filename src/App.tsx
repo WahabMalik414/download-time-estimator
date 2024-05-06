943544fde9450b0af938fcf362d1ce89bfb8dbaa
WahabMalik414 <malikwahab34@gmail.com>
2024-05-06 15:36:50 +0500

    added buttons

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,52 @@ function App() {
             </a>
           </div>
         </div>
+        <div className="bg-gray-300 mx-auto my-10 py-5">
+          <h1 className="text-center font-bold text-2xl">
+            Size of the file you want to download?
+          </h1>
+          <div className="flex flex-row gap-x-4 justify-center items-center mt-5">
+            <button
+              onClick={() => handleSpeedClick("add")}
+              className="btn text-3xl flex items-center justify-center hover:bg-green-200 h-14 w-14 bg-white"
+            >
+              +
+            </button>
+            <input
+              type="number"
+              placeholder="Speed"
+              value={speed}
+              onChange={(e) => setSpeed(parseFloat(e.target.value))}
+              className="input input-bordered w-32 h-14 rounded-lg text-xl text-center"
+            />
+
+            <button
+              onClick={() => handleSpeedClick("subtract")}
+              className="btn text-3xl flex items-center justify-center hover:bg-red-200 h-14 w-14 bg-white"
+            >
+              -
+            </button>
+            <select className="select select-bordered w-18 h-14 text-xl">
+              <option className="text-xl">mb</option>
+              <option className="text-xl">gb</option>
+              <option className="text-xl">tb</option>
+            </select>
+          </div>
+          <div className="flex justify-center mt-8">
+            <div
+              className="button w-40 h-16 bg-blue-500 rounded-lg cursor-pointer select-none
+    active:translate-y-2  active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+    active:border-b-[0px]
+    transition-all duration-150 [box-shadow:0_10px_0_0_#1b6ff8,0_15px_0_0_#1b70f841]
+    border-b-[1px] border-blue-400
+  "
+            >
+              <span className="flex flex-col justify-center items-center h-full text-white font-bold text-lg ">
+                Calculate
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
